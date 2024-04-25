859d64fa38a050838ddf54f33da3ae890cf81d82
min <dr94406@naver.com>
2024-04-25 16:47:57 +0900

    feat: Business Layer 테스트 3 마무리

--- src/main/java/sample/cafekiosk/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/api/service/order/OrderService.java
@@ -12,12 +12,14 @@ import sample.cafekiosk.spring.domain.product.ProductType;
 import sample.cafekiosk.spring.domain.stock.Stock;
 import sample.cafekiosk.spring.domain.stock.StockRepository;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class OrderService {
@@ -32,38 +34,58 @@ public class OrderService {
 
     List<Product> products = findProductBy(productNumbers);
 
-    // 재고 차감 체크가 필요한 상품들 filter
-    List<String> stockProductNumbers =
-        products.stream().filter(product -> ProductType.containsStockType(product.getProductType()))
-            .map(Product::getProductNumber).collect(Collectors.toList());
-
-    // 재고 엔티티 조회
-    List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
-
-    Map<String, Stock> stockMap =
-        stocks.stream().collect(Collectors.toMap(Stock::getProductNumber, s -> s));
-
-    // 상품별 counting
-    Map<String, Long> productCountingMap = stockProductNumbers.stream().collect(Collectors.groupingBy(p -> p, Collectors.counting()));
-
-    // 재고 차감 시도
-    for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
-        Stock stock = stockMap.get(stockProductNumber);
-        int quantity = productCountingMap.get(stockProductNumber).intValue();
-        if(stock.isQuantityLessThen(quantity)) {
-            throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
-        }
-        stock.deductQuantity(quantity);
-    }
+      deductStockQuantities(products);
 
 
-    Order order = Order.create(products, registeredDateTime);
+      Order order = Order.create(products, registeredDateTime);
     Order saveOrder = orderRepository.save(order);
     return OrderResponse.of(saveOrder);
 
   }
 
-  private List<Product> findProductBy(List<String> productNumbers) {
+    private void deductStockQuantities(List<Product> products) {
+        List<String> stockProductNumbers = extractsStockProductNumbers(products);
+
+        Map<String, Stock> stockMap = createStockMapBy(stockProductNumbers);
+
+        Map<String, Long> productCountingMap = createCountingMapBy(stockProductNumbers);
+
+        // 재고 차감 시도
+        for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
+            Stock stock = stockMap.get(stockProductNumber);
+            int quantity = productCountingMap.get(stockProductNumber).intValue();
+
+            if(stock.isQuantityLessThen(quantity)) {
+                throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+            }
+            stock.deductQuantity(quantity);
+        }
+    }
+
+    private static Map<String, Long> createCountingMapBy(List<String> stockProductNumbers) {
+        // 상품별 counting
+        return stockProductNumbers.stream()
+                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+    }
+
+    private Map<String, Stock> createStockMapBy(List<String> stockProductNumbers) {
+        // 재고 엔티티 조회
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+
+        Map<String, Stock> stockMap =
+            stocks.stream().collect(Collectors.toMap(Stock::getProductNumber, s -> s));
+        return stockMap;
+    }
+
+    private static List<String> extractsStockProductNumbers(List<Product> products) {
+        // 재고 차감 체크가 필요한 상품들 filter
+        List<String> stockProductNumbers =
+            products.stream().filter(product -> ProductType.containsStockType(product.getProductType()))
+                .map(Product::getProductNumber).collect(Collectors.toList());
+        return stockProductNumbers;
+    }
+
+    private List<Product> findProductBy(List<String> productNumbers) {
     List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
     Map<String, Product> productMap =
         products.stream().collect(Collectors.toMap(Product::getProductNumber, p -> p));
@@ -72,6 +94,5 @@ public class OrderService {
         productNumbers.stream()
             .map(productMap::get)
             .collect(Collectors.toList());
-
   }
 }
--- src/test/java/sample/cafekiosk/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/api/service/order/OrderServiceTest.java
@@ -18,8 +18,8 @@ import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
+
+import static org.assertj.core.api.Assertions.*;
 import static sample.cafekiosk.spring.domain.product.ProductType.*;
 import static sample.cafekiosk.spring.domain.product.SellingType.*;
 
@@ -47,6 +47,7 @@ class OrderServiceTest {
     orderProductRepository.deleteAllInBatch();
     productRepository.deleteAllInBatch();
     orderRepository.deleteAllInBatch();
+    stockRepository.deleteAllInBatch();
 
   }
 
@@ -162,6 +163,33 @@ class OrderServiceTest {
             tuple("001", 1000),
             tuple("001", 1000)
         );
+  }
+
+  @DisplayName("재고가 부족한 상품으로 주문을 생성하려는 경우 예외가 발생한다.")
+  @Test
+  void createOrderWithNoStock() {
+
+    LocalDateTime registeredDateTime = LocalDateTime.now();
+
+    // given
+    Product product = createProduct(BOTTLE, "001", 1000);
+    Product product2 = createProduct(BAKERY, "002", 3000);
+    Product product3 = createProduct(HANDMADE, "003", 5000);
+    productRepository.saveAll(List.of(product, product2, product3));
+
+    Stock stock1 = Stock.create("001", 2);
+    Stock stock2 = Stock.create("002", 2);
+    stock1.deductQuantity(1);
+    stockRepository.saveAll(List.of(stock1, stock2));
+
+    OrderCreateRequest request = OrderCreateRequest.builder()
+            .productNumbers(List.of("001", "001","002","003"))
+            .build();
+
+    // when // then
+    assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("재고가 부족한 상품이 있습니다.");
   }
 
   private Product createProduct(ProductType type, String productNumber, int price) {
